ba9f7fe3e2ab4697d4ef987f5374de5e4403c3fd
CopieChloe <chloe.seiler123@live.fr>
2019-05-23 02:52:11 +0200

    end of design

---
+++ main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+    $('.down_teaching').click(function() {
+        $('.activity_content_teaching').slideDown();
+    });
+
+    $('.up_teaching').click(function() {
+        $('.activity_content_teaching').slideUp();
+    });
+
+    $('.down_theater').click(function() {
+        $('.activity_content_theater').slideDown();
+    });
+
+    $('.up_theater').click(function() {
+        $('.activity_content_theater').slideUp();
+    });
+
+
+
+
+
+
+
+    
+
+});
